36fde7a920bc9a7532053a4905d95526d4f50c8d
Mahmoud-Eid9 <62315812+Mahmoud-Eid9@users.noreply.github.com>
2025-09-16 22:53:16 +0300

    Update express.d.ts

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -2,7 +2,6 @@ import 'express';
 
 declare module 'express-serve-static-core' {
   interface Request {
-    csrfToken(): string;          // CSRF token method
     user?: { sub: string; name: string }; // Optional user object
   }
 }
